96350cc2268b442ea7b837ef9267d21aa2075c2d
HpHiru <hirenp16895@gmail.com>
2022-05-05 19:51:07 +0530

    "done"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
-import { BrowserRouter } from 'react-router-dom';
-
-import Header from './Header';
+import logo from './logo.svg'
+import './App.css'
+import { Button } from 'react-bootstrap'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Header from './Header'
+import AddProduct from './AddProduct'
+import UpdateProduct from './UpdateProduct'
+import Login from './Login'
+import Register from './Register'
 
 function App() {
     return (
-        <div className="App" >
+        <div className="App">
             <BrowserRouter>
-            <Header />
+                <Header />
+                <Routes>
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/update" element={<UpdateProduct />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+                </Routes>
             </BrowserRouter>
         </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,10 @@ function Header() {
             <Navbar bg="primary" variant="dark">
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto nav-warper">
-                        <Link to='/add'>Add Product</Link>
-                        <Link to='/update'>Update Product</Link>
-                        <Link to='/login'>Login</Link>
-                        <Link to='/register'>Register</Link>
+                        <Link to="/add" >Add Product</Link>
+                        <Link to="/update" >Update Product</Link>
+                        <Link to="/login" >Login</Link>
+                        <Link to="/register" >Register</Link>
                     </Nav>
             </Navbar>
         </div>
