c28155d207ffd0b2a3c9643362edf23849fc0213
HpHiru <hirenp16895@gmail.com>
2022-05-06 01:41:02 +0530

    "done api save"

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,58 @@
-function Register (){
-    return(
-        <div>Register Page</div>
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useNavigate();
+    async function signUp() {
+        let item = { name, email, password }
+        let result = await fetch('http://localhost:8000/api/register', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(item)
+        });
+        result = await result.json();
+        localStorage.setItem("user-info", JSON.stringify(result));
+        history('/add');
+
+    }
+    return (
+        <div>
+            <Row>
+                <Col md={4}>
+                </Col>
+                <Col md={4}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter name" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                        </Form.Group>
+                        <Button onClick={signUp} variant="primary" type="button">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+                <Col md={4}>
+                </Col>
+            </Row>
+        </div>
     )
 }
 export default Register
